ee9b56ad4c49d95b84fd3149dd1051dcebf26dd0
ADUB <rootbeer608@gmail.com>
2022-10-14 16:55:35 -0700

    backedn eslint configured and fixed

--- backend/routes/api/addToPlaylist.js
+++ backend/routes/api/addToPlaylist.js
@@ -1,13 +1,7 @@
-const express = require('express');
-const asyncHandler = require('express-async-handler');
+const express = require("express");
 
-const { check } = require('express-validator');
-const { handleValidationErrors } = require('../../utils/validation');
-
-const db = require('../../db/models');
 const router = express.Router();
 
 //ADD TO PLAYLIST
-
 
 module.exports = router;
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,30 +1,23 @@
-const router = require('express').Router();
-const sessionRouter = require('./session.js');
-const usersRouter = require('./users.js');
-const songsRouter = require('./songs.js')
-const playlistsRouter = require('./playlists')
- const SongPlaylistRouter = require('./songPlaylist.js')
+const router = require("express").Router();
+const sessionRouter = require("./session.js");
+const usersRouter = require("./users.js");
+const songsRouter = require("./songs.js");
+const playlistsRouter = require("./playlists");
+const SongPlaylistRouter = require("./songPlaylist.js");
 
-const { restoreUser } = require('../../utils/auth.js');
-const playlist = require('../../db/models/playlist.js');
-router.get(
-  '/restore-user',
-  restoreUser,
-  (req, res) => {
-    return res.json(req.user);
-  }
-);
+const { restoreUser } = require("../../utils/auth.js");
+router.get("/restore-user", restoreUser, (req, res) => {
+  return res.json(req.user);
+});
 
-router.use('/session', sessionRouter);
+router.use("/session", sessionRouter);
 
-router.use('/users', usersRouter);
-router.use('/songs', songsRouter);
-router.use('/playlists', playlistsRouter)
-router.use('/songplaylist', SongPlaylistRouter)
+router.use("/users", usersRouter);
+router.use("/songs", songsRouter);
+router.use("/playlists", playlistsRouter);
+router.use("/songplaylist", SongPlaylistRouter);
 
-
-
-router.post('/test', (req, res) => {
+router.post("/test", (req, res) => {
   res.json({ requestBody: req.body });
 });
 
