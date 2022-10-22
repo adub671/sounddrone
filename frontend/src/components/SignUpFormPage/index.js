eae4384aea11a8ac0ba7fcde9dd725af1ad81ad9
ADUB <rootbeer608@gmail.com>
2022-10-21 20:25:00 -0700

    added sign up validation

--- frontend/src/components/SignUpFormPage/index.js
+++ frontend/src/components/SignUpFormPage/index.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 import * as sessionActions from "../../store/session";
-import './SignUpForm.css';
+import "./SignUpForm.css";
 
 function SignupFormPage() {
   const dispatch = useDispatch();
@@ -16,73 +16,108 @@ function SignupFormPage() {
 
   if (sessionUser) return <Redirect to="/" />;
 
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (password === confirmPassword) {
-      setErrors([]);
-      return dispatch(sessionActions.signup({ email, username, password }))
-        .catch(async (res) => {
-          const data = await res.json();
-          if (data && data.errors) setErrors(data.errors);
-        });
+    const errs = [];
+    if (!validateEmail(email)) {
+      errs.push("Please enter a valid email");
     }
-    return setErrors(['Confirm Password field must be the same as the Password field']);
+    if (validateEmail(username)) {
+      errs.push("Username cannot be an email");
+    }
+    if (username.length > 4 || username.length < 30) {
+      errs.push("Please provide a username of between 4 and 30 characters");
+    }
+    if (password.length < 6) {
+      errs.push("Password must be 6 characters or more");
+    }
+    if (password !== confirmPassword) {
+      errs.push(
+        "Confirm Password field must be the same as the Password field"
+      );
+    }
+    setErrors(errs);
+    if (errs.length === 0) {
+      setErrors([]);
+      return dispatch(
+        sessionActions.signup({ email, username, password })
+      ).catch(async (res) => {
+        const data = await res.json();
+        if (data && data.errors) setErrors(data.errors);
+      });
+    }
   };
 
   return (
     <>
-    <div className="page-container">
-    <div className="content-container">
-    <div className="header"><h1>Sign Up</h1></div>
-    
-    <form onSubmit={handleSubmit}>
-      <ul>
-        {errors.map((error, idx) => <li key={idx}>{error}</li>)}
-      </ul>
-      <label>
-        Email
-        <input
-          type="text"
-          className="signup-input"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Username
-        <input
-          type="text"
-          className="signup-input"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Password
-        <input
-          type="password"
-          className="signup-input"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Confirm Password
-        <input
-          type="password"
-          className="signup-input"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          required
-        />
-      </label>
-      <button id="signup-button" type="submit">Sign Up</button>
-    </form>
-    </div>
-    </div>
+      <div className="page-container">
+        <div className="content-container">
+          <div className="header">
+            <h1>Sign Up</h1>
+          </div>
+
+          <form onSubmit={handleSubmit}>
+            <ul>
+              {errors.map((error, idx) => (
+                <li key={idx} className={"validation-error"}>
+                  {error}
+                </li>
+              ))}
+            </ul>
+            <label>
+              Email
+              <input
+                type="text"
+                className="signup-input"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </label>
+            <label>
+              Username
+              <input
+                type="text"
+                className="signup-input"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+            </label>
+            <label>
+              Password
+              <input
+                type="password"
+                className="signup-input"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </label>
+            <label>
+              Confirm Password
+              <input
+                type="password"
+                className="signup-input"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                required
+              />
+            </label>
+            <button id="signup-button" type="submit">
+              Sign Up
+            </button>
+          </form>
+        </div>
+      </div>
     </>
   );
 }
